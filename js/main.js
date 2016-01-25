a0bf4f75aad02fb58b12d3cdebdaec263f5a4000
Matthew Yu <mattyu007@gmail.com>
2016-01-25 01:36:13 -0500

    Added work history

--- js/main.js
+++ js/main.js
@@ -2,12 +2,11 @@
 
 window.addEventListener('load', function() {
     var quotes = [
-        'Probably.',
         'Nyeh heh heh.',
-        // 'SO hyped for the destruction of humanity.', /* Catty is too extreme for most people. */
+        // 'SO hyped for the destruction of humanity.', /* Catty is too extreme for most. */
         '“Because one day… I’m going to befriend one of them.”'
     ];
     
-    document.getElementById('random-quote').title =
+    document.getElementById('intro').title =
         quotes[Math.floor(Math.random() * quotes.length)];
 });
